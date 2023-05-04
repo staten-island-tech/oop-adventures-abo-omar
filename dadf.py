4f40ca2d32d4d5840ebdc2ac54205634912c52da
wabdallah889 <124072154+wabdallah889@users.noreply.github.com>
2023-05-03 23:54:36 -0400

    adfsadfdafgs
    gdaadgdag

--- dadf.py
+++ dadf.py
@@ -26,3 +26,27 @@ class Character:
 
     def __str__(self):
         return self.name
+
+
+
+
+
+class Game:
+    def __init__(self):
+        self.score = 0
+        self.current_room = None
+
+    def start(self):
+        print("welcome to the pyramid of giza habibi ")
+        self.setup_game()
+
+    def setup_game(self):
+        # Initialize rooms
+        room1 = Room("starting room", "its a starting room.", )
+        room2 = Room("hallway", "a long dark hallway could contain monsters or bodies who knows .", [Item("british short pistol", "An old british pistol probably left from an expedition during the early 1900s.", 20)])
+        room3 = Room("queens chamber ", "a very dark room seems pretty big you can notice movement in there .", [Item("ancient candle", "its an ancient candle .", 2)])
+        room2.items.append(Character("a mummy", "he came back from the dead to just kill you.", -5))
+        room3.items.append(Character("queen ice spice", "you thought she was feeling you????.", -20))
+
+        # Set starting room
+        self.current_room = room1
