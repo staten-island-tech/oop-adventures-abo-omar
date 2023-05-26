d32e7400ae5a00ebbf1dc99b86b02ff3579423ae
wabdallah889 <124072154+wabdallah889@users.noreply.github.com>
2023-05-26 10:51:58 -0400

    ewsgfgjgfdjsdjg
    jgdgdjdgjjsdg

--- dadf.py
+++ dadf.py
@@ -55,7 +55,7 @@ class Game:
    
     def __init__(self):
        
-        score=[0]
+        self.score = 0
         
         self.current_room=None
 
@@ -85,24 +85,24 @@ class Game:
         room3.items.append(Character("queen ice spice", "you thought she was feeling you????.", -20))
 
         
-        self.current_roomcurrent_room = room1
+        self.current_room = room1
         pistol=[[Item("british short pistol", "An old british pistol probably left from an expedition during the early 1900s.", 20)]]
         inventory=[]
         sharpweapons=[[Item("Khanjar", "its a curved arab sword .", 25)] , [Item("psychte", "its a psychte .", 25)]]
         
-        x = 1
+        print("\nYou are in the", self.current_room.name)
+           
+        print(self.current_room.description)
+           
+        print(self.current_room.items)
+        
+        print(self.score)
 
         #start game loop  
         
-        while x == 1:
-           
-            print("\nYou are in the", self.current_room.name)
-           
-            print(self.current_room.description)
-           
-            print(self.current_room.items)
-           
-            print(self.score)
+        while True:
+            
+    
             
             action = input ("Enter your action (type 'help' for options'): ") 
             
@@ -138,9 +138,6 @@ class Game:
                
                 inventory.append (self.current_room.items)
            
-            elif action.startswith('interact'):
-               
-                print(self.current_room.character)
             
             elif action.startswith('room6'):
                
@@ -163,16 +160,28 @@ class Game:
                         z = input("in your never ending journey and seeming for going in circles for god knows how long you lost track of the concept of time you decide your are tired and decide wether or not you should rest do you keep going yes or no ")
                         
                         if z.startswith('yes'):
+                           
+                            
+                            
                             print('in the end you decide to keep going but you felt more and more tired by the second then out of nowhere you see a light shinning on you telling you come join me you were hallucinating  in the end you collapsed and died your body was never found to this day ')
                         
+                            print(self.score)
+
+                            quit
                         elif z.startswith('no'):
                            
                             x = input('you decide to find a place to rest and you keep going and you get very tired do you keep looking or just sit on the floor ')
                             
                             if x.startswith('keep looking'):
                                
+                               
+                                
                                 print('well you keep going being the germaphobe you are now and disgusted on the flor ou keep going good job in the end you pass out and die how eciting')
-                           
+                                
+                                print(self.score)
+
+                                quit
+                            
                             elif x.startswith('sit') :
                                
                                 L = input('you decide to sit down at the wall  an drest for a bit butt as sit down and put your back on the wall you felt the  wall move a bit and then you feel the ground shake what do you do investiagte or keep resting  ')
@@ -188,11 +197,19 @@ class Game:
                                         if P.startswith('Keep exploring'):
                                            
                                             print('you decide to keep going and the adrenaline wears and you collapse on the floor and never wake up boo hoo you died what did you expect when you overstress for a tired body')
+
+                                            print(self.score)
+
+                                            quit
                                         
                                         elif P.startswith('continue resting'):
                                             
                                             print('you decide to sit down and keep ressting and then you see a light you can escape you finally can getout as you approach the light you have a warm feeling in your heart and you get happier and happier until it all turns to black  ')
-                                    
+                                            
+                                            print(self.score)
+
+                                            quit
+                                   
                                     elif T.startswith('investigate'):
                                         
                                         Q = input('you decide to get up and investigate and then you found a new room never seen before  you find the screching noise to be a strong flow of air  what do you do start diiging a hole out of where the air is coming out of or find a tool to help you with it ')
@@ -201,6 +218,10 @@ class Game:
                                            
                                             print('you decide to start right away and keep digiing with your hand and the whole gets bigger and bigger and you see its night out how long has it been it doesnt matter for now freedomn is right here until you feel something weird and then a sharp stinging pain on your right hand and it hurt you take it out and see scorpions crawl out and next thing you know you fall on the ground and nothing else for it all turned to black')
                                       
+                                            print(self.score)
+
+                                            quit
+                                        
                                         elif Q.startswith('look for a tool'):
                                            
                                             U = input('you decide to look in the room for something to make a hole with and ge out you keep looking and looking but then you find two things an old bone that seems sturdy and very old worn out shovel which do you choose')
@@ -208,7 +229,11 @@ class Game:
                                             if U.startswith('shovel'):
 
                                                 print('you decide to take the shove and you start digging then after a few rounds the shovl snaps in half and venemous spiders start crawling out of it guess it is a bad day to have arachnaphobia ')
+                                                
+                                                print(self.score)
 
+                                                quit
+                                            
                                             elif U.startswith('bone'):
                                                
                                                 print('you pick up to bone and start smashing a hole where the air is and it gets bigger and bigger then you notice scorpions crawling out they dont attack you tho then you hear the roof crack and next you know everything turns black your body was found a day later by locals due yo yhem noticing a huge hole in the sand near the pyramids your bdy was crushed in the procces though') 
@@ -226,18 +251,32 @@ class Game:
                                             if pistol in inventory:
                                                 
                                                 print('you try to scream for help your finally out now but then after a few minutes nothing happens so you stop until you notice movement you get excited but then its gets closer and closer and you see its a large snake but luckily you were quick enough to pull out your pistol and shoot it who knew you would have a use for that pistol you found  but shortly after you hear people screaming and running towards you yay your rescued you get found by the locals and they were able to get an exicvator to get you out next thing you know your all over the news man isnt egypt just great')
+                                            
+                                                print(self.score)
+                                               
+                                                quit
+                                            
                                             else :
                                                 print(' you try to scream for help your finally out now but then after a few minutes nothing happens so you stop until you notice movement you get excited but then its gets closer and closer and you see its a large snake what did you expect in a dessert') 
-                                        
+                                                
+                                                print(self.score)
+                                               
+                                                quit
                                         elif M.startswith('cut'):
 
                                             if sharpweapons in inventory :
 
                                                 print('you try to cut your leg off with the sharpweaposn you found in the pyramid and you do succsefully you then run off into civlization where the people see you and approach you when they see what happend they rush to a hospital your saved yay too bad theres no insurance for you in egypt ')
-
+                                                
+                                                print(self.score)
+                                               
+                                                quit
                                             else :
                                                 print('you tried to cut your leg off but nothing really worked  so you just sit there hopelessly and think over your life and the actions you have done what a life you say as you die of a heat stroke buit finnaly at peace with yourself')
-                                            
+
+                                                print(self.score)
+                                               
+                                                quit
 
                                             
                                             
@@ -249,9 +288,10 @@ class Game:
             else:
                
                 print("you broke it habibi thats not good")
-    __init__(0)
-    start(0)
-    setup_game(0)
+    
+game = Game()
+game.setup_game()
+
 
 
 
